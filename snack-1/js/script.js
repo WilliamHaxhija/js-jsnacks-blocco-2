3e75cb85dd3b93b19e12551b961a52fce10ce83b
WilliamHaxhija <williamhaxhija@gmail.com>
2024-03-12 19:07:40 +0100

    feat: add snack-1

--- snack-1/js/script.js
+++ snack-1/js/script.js
@@ -1,1 +1,13 @@
 //L'utente inserisce un numero nel prompt, se è pari stampa il numero, se è dispari stampa il numero successivo
+
+//Chiedo un numero all'utente con un prompt
+  //Se è pari, lo stampo in un p
+  //Altrimenti stampo il numero successivo
+
+  let userNumber = parseInt(prompt('Inserisci un umero'));
+  if (userNumber % 2 === 0) {
+    document.querySelector('#even-number').innerHTML = userNumber;
+  } else {
+    userNumber++;
+    document.querySelector('#even-number').innerHTML = userNumber;
+  }
