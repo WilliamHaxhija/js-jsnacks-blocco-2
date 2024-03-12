53627a6ce3465fe7dbdd4b9ad7c0f457be09f6cc
WilliamHaxhija <williamhaxhija@gmail.com>
2024-03-12 20:24:19 +0100

    feat: add BONUS and end project

--- BONUS/js/script.js
+++ BONUS/js/script.js
@@ -1,1 +1,9 @@
 //Chiedi un numero di 4 cifre all’utente e calcola la somma di tutte le cifre che compongono il numero.
+
+const userNumber = prompt('Digita un numero di 4 cifre');
+console.log(userNumber);
+let sum = 0;
+for (let i = 0; i < userNumber.length; i++) {
+    sum += parseInt(userNumber[i]);
+}
+console.log(sum);
